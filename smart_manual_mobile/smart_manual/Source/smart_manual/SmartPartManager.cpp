f76ef1d41394f201492d074f81eceb44a9529d91
BATMINJI <minjiconan@naver.com>
2025-09-06 20:34:53 +0900

    Smart Manual Manager 클래스 추가 <- 모바일 UI 및 컨트롤러 완성 되면 시험 가능할 듯?

---
+++ smart_manual_mobile/smart_manual/Source/smart_manual/SmartPartManager.cpp
@@ -0,0 +1,78 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SmartPartManager.h"
+
+// Sets default values
+ASmartPartManager::ASmartPartManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	CurrentIndex = -1;
+}
+
+// Called when the game starts or when spawned
+void ASmartPartManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	DeactivateAll();
+}
+
+void ASmartPartManager::ActivatePart(int32 Index)
+{
+    if (!PartList.IsValidIndex(Index))
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Invalid part index: %d"), Index);
+        return;
+    }
+
+    if (PartList.IsValidIndex(CurrentIndex))
+    {
+        PartList[CurrentIndex]->SetActorTickEnabled(false);
+    }
+
+    CurrentIndex = Index;
+    PartList[CurrentIndex]->SetActorTickEnabled(true);
+
+    UE_LOG(LogTemp, Log, TEXT("Activated part index: %d"), CurrentIndex);
+}
+
+void ASmartPartManager::NextPart()
+{
+    int32 NewIndex = CurrentIndex + 1;
+    if (PartList.IsValidIndex(NewIndex))
+    {
+        ActivatePart(NewIndex);
+    }
+    else
+    {
+        UE_LOG(LogTemp, Log, TEXT("Already at last part."));
+    }
+}
+
+void ASmartPartManager::PrevPart()
+{
+    int32 NewIndex = CurrentIndex - 1;
+    if (PartList.IsValidIndex(NewIndex))
+    {
+        ActivatePart(NewIndex);
+    }
+    else
+    {
+        UE_LOG(LogTemp, Log, TEXT("Already at first part."));
+    }
+}
+
+void ASmartPartManager::DeactivateAll()
+{
+    for (ASmartPartActor* Part : PartList)
+    {
+        if (Part)
+        {
+            Part->SetActorTickEnabled(false);
+        }
+    }
+    CurrentIndex = -1;
+}
